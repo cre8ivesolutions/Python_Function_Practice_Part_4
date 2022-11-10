3cc6abbc19e7fcb0be75030acc10657d8197ad87
cre8ivesolutions <tammerz78@gmail.com>
2022-11-10 11:53:31 -0600

    complete

--- practice_part4.py
+++ practice_part4.py
@@ -2,6 +2,8 @@
 def max_num(a,b,c):
   return max([a,b,c])
 
+print(f"The max_num is {max_num(3,4,5)}")
+
 # Write a Python function called mult_list() to multiply all the numbers in a list.
 def mult_list(lst):
   if len(lst) == 0:
@@ -16,19 +18,22 @@ def mult_list(lst):
 
   return prod
 
+print(f"The mult_list is {mult_list([5,7])}")
+
 # Write a Python function called rev_string() to reverse a string.
 def rev_string(my_str):
-  return my_str[::-1]
+  print (my_str[::-1])
+
+rev_string('This is backwards. >-- sdrawkcab si sihT')
 
 # Write a Python function called num_within() to check whether a number falls in a given range.
-# The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
-# Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, num_within(10,2,5) returns False.
+
 def num_within(x,a,b):
   return x in range(a,b+1)
+
      
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
 # The function accepts the number n, the number of rows to print
-# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
 
 triangle = [[1],[1,1]]
 def pascal(n):
@@ -61,3 +66,8 @@ def pascal(n):
     #print triangle
     for row in triangle:
       print(row)
+
+print("Below is Pascal with 5 rows: ")
+pascal(5)
+
+
